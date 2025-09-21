2054fce281fe7944a01873f1a9b3c2e9742b44ec
Gyumin Sim <gyumin.sim@privatenote.co.kr>
2025-09-21 22:11:45 +0900

    Add more require statements

--- lib/google/genai.rb
+++ lib/google/genai.rb
@@ -6,6 +6,15 @@ loader.setup
 
 require_relative "genai/version"
 require_relative "genai/api_client"
+require_relative "genai/models"
+require_relative "genai/chats"
+require_relative "genai/files"
+require_relative "genai/tunings"
+require_relative "genai/caches"
+require_relative "genai/batches"
+require_relative "genai/operations"
+require_relative "genai/tokens"
+require_relative "genai/live"
 require_relative "genai/client"
 
 module Google
